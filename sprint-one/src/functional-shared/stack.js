b060daf6982b2c181734e808fcf84f5a1290ffca
rebeccaryang <rebeccayang123@gmail.com>
2015-11-18 15:43:58 -0800

    Functional Shared: Allows for adding and removing

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -10,13 +10,19 @@ var Stack = function() {
 var stackMethods = {
   push:function(val){
     this.stackSize++;
+    this[this.stackSize] = val;
+    console.log(this)
+    console.log(this.stackSize);
   },
   size:function(){
     return this.stackSize;
   },
   pop:function(){
     if(this.stackSize > 0){
+      var lastIn = this[this.stackSize];
+      delete this[this.stackSize];
       this.stackSize--;
+      return lastIn;
     }
   }
 };
