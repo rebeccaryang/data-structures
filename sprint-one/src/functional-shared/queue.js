26cc783fc68843d95e08578d1349d7851fad444b
rebeccaryang <rebeccayang123@gmail.com>
2015-11-18 15:49:36 -0800

    FUNCTIONAL SHARED - Returns 0 for new queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.queueSize = 0;
+  obj.beginning = 0;
+  obj.end = 0;
+
+  _.extend(obj,queueMethods);
+  return obj;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue:function(val){
+
+  },
+  dequeue: function(){
+
+  },
+  size: function(){
+    return this.queueSize;
+  }
+};
 
 
