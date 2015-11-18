70e86c212445962e7af7f0b6170297a612aa9281
rebeccaryang <rebeccayang123@gmail.com>
2015-11-18 15:33:51 -0800

    Functional Shared: increments size on push, and returns size of 0 for new

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.stackSize = 0;
+  _.extend(obj,stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push:function(val){
+    this.stackSize++;
+  },
+  size:function(){
+    return this.stackSize;
+  }
+};
 
 
