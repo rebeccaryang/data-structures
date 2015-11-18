b006550f88566ce92bed239637609eff628d4664
rebeccaryang <rebeccayang123@gmail.com>
2015-11-18 15:25:59 -0800

    FUNCTIONAL stack and queue built

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,8 @@
 var Queue = function() {
   var someInstance = {};
+  var queueSize = 0;
+  var beginning = 0;
+  var end = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +10,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    queueSize++;
+    storage[end] = value;
+    end++;
   };
 
   someInstance.dequeue = function() {
+    if(queueSize>0){
+      queueSize--;
+      var dequeuedValue = storage[beginning];
+      delete storage[beginning];
+      beginning++;
+      return dequeuedValue;
+    };
+
   };
 
   someInstance.size = function() {
+    return queueSize;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,31 @@
 var Stack = function() {
   var someInstance = {};
+  var stackSize = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //assign something to someInstance(stackSize);
+    someInstance[stackSize] = value;
+    stackSize++;
+
+    //should add stuff into Instance
   };
 
   someInstance.pop = function() {
+    //return someInstance(stackSize)
+      //if zero then should not subtract, otherwise stackSize becomes negative
+    stackSize--;
+    var poppedValue = someInstance[stackSize];
+    delete someInstance[stackSize];
+    return poppedValue;
+    //should subtract stuff from instance and delete key
   };
 
   someInstance.size = function() {
+    return stackSize>=0 ? stackSize : 0;
   };
 
   return someInstance;
