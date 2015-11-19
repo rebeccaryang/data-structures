9375a6d5351d22cd95da5e17449dfb2829775a8c
rebeccaryang <rebeccayang123@gmail.com>
2015-11-18 18:23:02 -0800

    PSEUDOCLASSICAL - Stack/Queue built

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.queueSize = 0;
+  this.beginning = 1;
+  this.end = 0;
 };
+
+Queue.prototype.enqueue = function(val){
+  this.end++;
+  this.queueSize++;
+  this[this.end] = val;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.queueSize > 0){
+    var dequeued = this[this.beginning];
+    delete this[this.beginning];
+    this.beginning++;
+    this.queueSize--;
+    return dequeued;
+  }
+};
+
+Queue.prototype.size = function(){
+    return this.queueSize;
+};
+
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,45 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
+  //debugger;
+
+};
+/*
+Stack.prototype = {
+  pop: function(){
+    if(this.stackSize>0){
+      var poppedOff = this[this.stackSize];
+      delete this[this.stackSize];
+      this.stackSize--;
+      return poppedOff;
+
+    }
+  },
+  push: function(val){
+    this.stackSize++;
+    this[this.stackSize] = val;
+  },
+  size: function(){
+    return this.stackSize;
+  }
 };
 
+*/
+Stack.prototype.pop = function(){
+    if(this.stackSize>0){
+      var poppedOff = this[this.stackSize];
+      delete this[this.stackSize];
+      this.stackSize--;
+      return poppedOff;
 
+    }
+  };
+Stack.prototype.push = function(val){
+    this.stackSize++;
+    this[this.stackSize] = val;
+  };
+
+Stack.prototype.size = function(){
+    return this.stackSize;
+  };
