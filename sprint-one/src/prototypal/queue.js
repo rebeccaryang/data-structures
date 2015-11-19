c7b19383204788d0d41c0cbea4b77f64cd1621af
rebeccaryang <rebeccayang123@gmail.com>
2015-11-18 16:08:44 -0800

    PROTOTYPICAL - Returns size 0 for new queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,19 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.queueSize = 0;
+  obj.beginning = 1;
+  obj.end = 0;
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue:function(){},
+  dequeue:function(){},
+  size:function(){
+    return this.queueSize;
+  }
+};
 
 
