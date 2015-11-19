166cacc06e8e51423a657d06f70dacedecb32515
rebeccaryang <rebeccayang123@gmail.com>
2015-11-19 11:34:50 -0800

    Completes tree sprint

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,12 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree,treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+
+
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -12,12 +15,26 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // your code here
-  newTree.children = null;  // fix me
+  this.children.push(Tree(value));  // fix me
 };
 
 treeMethods.contains = function(target) {
+  var containsTarget = false;
+  var treeTraversal = function(obj){
+    if(obj.value === target){
+      containsTarget = true;
+      return;
+    } else if (obj.children.length === 0){
+      return;
+    } else {
+      _.each(obj.children,function(elem){
+        treeTraversal(elem);
+      })
+    }
+  }
+  treeTraversal(this);
+  return containsTarget;
 };
-
 
 
 /*
