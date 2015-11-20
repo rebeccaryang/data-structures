a2b945023d8981969339feb09fa0d6eddea6bf47
rebeccaryang <rebeccayang123@gmail.com>
2015-11-19 19:48:11 -0800

    Inserts and returns if Contains

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -2,15 +2,54 @@ var BinarySearchTree = function(value) {
   var bst = Object.create(methods);
   bst.left = null;
   bst.right = null;
+  bst.value = value;
   return bst;
 };
 
 var methods = {
   contains:function(target){
+    var containsTarget = false;
+    var containsCheck = function(tree){
+      if(tree.value===target){
+        containsTarget = true;
+        return;
+      }
+      else if(target < tree.value){
+        if(tree.left === null){
+          return;
+        }
+        else{
+          containsCheck(tree.left);  
+        }  
+      }
+      else if(target > tree.value){
+        if(tree.right === null){
+          return;
+        }
+        else{
+          containsCheck(tree.right);
+        }
+      }
+    };
+    containsCheck(this);
+    return containsTarget;
 
   },
   insert:function(value){
+    
+    var traversal = function(tree){
+      if(value > tree.value && tree.right === null){
+        tree.right = BinarySearchTree(value);
+      } else if (value > tree.value){
+        traversal(tree.right);
+      } else if (value < tree.value && tree.left === null){
+        tree.left = BinarySearchTree(value);
+      } else if (value < tree.value) {
+        traversal(tree.left);
+      }
+    }
 
+    traversal(this);
   },
   depthFirstLog:function(cb){
 
