edd477efca0fc71bb490564126175806dd50121f
rebeccaryang <rebeccayang123@gmail.com>
2015-11-20 17:56:03 -0800

    Adds tails, adds heads, and removes heads, and removes tails while accounting for cases when LL is empty

--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -14,28 +14,38 @@ var DoublyLinkedList = function() {
 
   list.addToTail = function(value) {
 
+    /*
     var traverseObject = function(object){
       if(object.next === null){
         var toBeAddedTail = Node(value);
         toBeAddedTail.previous = object;
         object.next = toBeAddedTail;
+        list.tail = toBeAddedTail;
+
       }
       else{
         traverseObject(object.next);
       };
     }
+    */
 
 
     if(list.head === null){
       var toBeAddedHeadNull = Node(value);
-      toBeAddedHeadNull.previous = null;
-      this.head = toBeAddedHeadNull;
+      // toBeAddedHeadNull.previous = null;
+      list.head = toBeAddedHeadNull;
+      list.tail = list.head;
 
     } else {
-      traverseObject(this.head);
+      //traverseObject(this.head);
+      var newNode = Node(value);
+      //var currentTail = this.tail;
+      list.tail.next = newNode;
+      newNode.previous = list.tail;
+      list.tail = newNode;
     }
 
-    this.tail = Node(value);
+    
   };
 
 
@@ -43,15 +53,22 @@ var DoublyLinkedList = function() {
     //var removed = this.head.value;
     //this.head = this.head.next;
     //return removed;
-    var removed = this.head.value;
-    this.head = this.head.next;
-    return removed;
+    if(this.head === null){
+      //return undefined;
+    } else {
+      var removed = this.head.value;
+      this.head = this.head.next;
+      if(this.head !== null) {
+       this.head.previous = null; 
+      }
+      return removed;
+    }
     //copy the contents of next
     //assign them to head
   };
 
   list.contains = function(target) {
-    var containsTarget = false;
+    /*var containsTarget = false;
     var traverseFind = function (object){
       if(object.value === target){
         containsTarget = true;
@@ -62,13 +79,36 @@ var DoublyLinkedList = function() {
       }
     }
     traverseFind(this.head);
-    return containsTarget;
+    return containsTarget;*/
+
+    //
+    var currentNode = this.head;
+    while(currentNode !== null){
+      if(currentNode.value === target){
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   list.removeTail = function(){
      //traverse down the list
+     if(list.head === null){
+
+     } else { 
+      var removed = list.tail.value;
+      list.tail = list.tail.previous;
+
+      return removed;
+     }
+     
+     //list.tail.next = null;
+     
+    /* var removedTail;
      var traverseRemoval = function(object){
         if(object.next === null){
+          removedTail = object.value;
           this.tail = object.previous;
           object.previous.next = null;
         } else {
@@ -77,6 +117,7 @@ var DoublyLinkedList = function() {
 
      }
      traverseRemoval(this.head);
+     return removedTail;*/
   };
 
   list.addToHead = function(value) {
@@ -84,11 +125,16 @@ var DoublyLinkedList = function() {
     //var temp = this.head;
     //var tempJSON = JSON.stringify(temp);
     //tempJSON = JSON.parse(tempJSON);
-    
-    toBeAdded.next = this.head;
-    this.head.previous = toBeAdded;
-    toBeAdded.previous = null;
-    this.head = toBeAdded;
+    if(this.head === null){
+      this.head = toBeAdded;
+      // NOTE FOR LATER -- We need to assign tail here :-)
+    } else {
+      toBeAdded.next = this.head;
+      toBeAdded.next.previous = toBeAdded;
+      toBeAdded.previous = null;
+      this.head = toBeAdded;
+    }
+
   };
 
   return list;
